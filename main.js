cdd0fb78162674b0e1cc71c96877ba6d5e0cfdfd
amomAA <chernovvova2016@yandex.ru>
2022-12-25 19:15:32 +0300

    a

--- main.js
+++ main.js
@@ -81,3 +81,24 @@ $(document).ready(function () {
         }
     });
 });
+
+$(function(){
+    $(".ajaxForm").submit(function(e){
+        e.preventDefault();
+        var href = $(this).attr("action");
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: $(this).serialize(),
+            success: function(response){
+                if(response.status == "success"){
+                    alert("Данные успешно отправлены!");
+                }
+                else{
+                    alert("Ошибка: " + response.message);
+                }
+            }
+        });
+    });
+});
